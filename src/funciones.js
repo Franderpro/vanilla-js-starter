a959d8e846e68db6886ce83dc22217c18478bd86
Franderpro <rfrander11@gmail.com>
2024-06-04 10:37:41 -0600

    prueba0,2

--- src/funciones.js
+++ src/funciones.js
@@ -7,9 +7,7 @@ let add = document.getElementById('crearTarea')
 add.addEventListener("click",function () {
   
     crearTarea()
-    extraerTarea()
-    eliminar()
-  
+    
 })
 
 
@@ -23,53 +21,6 @@ async function extraerTarea() {
         console.error("Error al realizar la solicitud:", error);
     }
 }
-
-function crearTarea() {
-    const tareaInput = document.getElementById("tarea");
-    const tarea = tareaInput.value.trim();
-    const tituloTarea = document.getElementById("tituloTarea");
-    const titulo = tituloTarea.value.trim();
-    if (tarea !== "") {
-        const texto = document.createElement("p");
-        texto.innerText = titulo + ": " + tarea;
-        const botonEliminar = document.createElement("button");
-        botonEliminar.innerText = "Eliminar";
-        botonEliminar.onclick = function () {
-            texto.parentNode.removeChild(texto);
-            eliminarUltimaTarea()
-            eliminar()
-        };
-        texto.appendChild(botonEliminar);
-        document.body.appendChild(texto);
-        guardarTareas({
-            titulo: titulo, 
-            description: tarea
-        });
-        tareaInput.value = "";
-    }
-}
-
-document.getElementById("eliminar").addEventListener("click", function () {
-    eliminarUltimaTarea();
-});
-
-function eliminarUltimaTarea() {
-    const elementos = document.querySelectorAll("p");
-    const ultimoElemento = elementos[elementos.length - 1];
-    if (ultimoElemento) {
-       
-        ultimoElemento.parentNode.removeChild(ultimoElemento);
-        extraerTarea()
-        eliminar(id);
-    }
-    fetch('http://localhost:3000/api/task/')
-            .then(response => response.json())
-            .then(id=> console.log(id));
-            eliminar();
-            eliminarUltimaTarea
-    
-}
-
 async function guardarTareas(nuevaTarea) {
     try {
         const response = await fetch("http://localhost:3000/api/task/", {
@@ -81,6 +32,8 @@ async function guardarTareas(nuevaTarea) {
         });
         if (response.ok) {
             const data = await response.json();
+            return data[data.length-1].id
+            
         } else {
             console.error("Error al guardar la tarea:", response.statusText);
         }
@@ -88,6 +41,32 @@ async function guardarTareas(nuevaTarea) {
         console.error("Error al realizar la solicitud:", error);
     }
 }
+ async function crearTarea() {
+    const tareaInput = document.getElementById("tarea");
+    const tarea = tareaInput.value.trim();
+    const tituloTarea = document.getElementById("tituloTarea");
+    const titulo = tituloTarea.value.trim();
+    if (tarea !== "") {
+        const texto = document.createElement("p");
+        texto.innerText = titulo + ": " + tarea;
+        const botonEliminar = document.createElement("button");
+        botonEliminar.innerText = "Eliminar";
+        botonEliminar.onclick = function () {
+            texto.parentNode.removeChild(texto);
+            eliminar(tarea.id)
+        };
+        texto.appendChild(botonEliminar);
+        document.body.appendChild(texto);
+        let id= await guardarTareas({
+            titulo: titulo, 
+            description: tarea
+
+        });
+        console.log(id);
+        tareaInput.value = "";
+    }
+}
+
 
 async function eliminar(id) { 
     try {
